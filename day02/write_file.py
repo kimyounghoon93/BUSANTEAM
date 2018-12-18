2e1058098088b8ce5ed0bb977d840b359a834c4e
Young Hoon <kyh2021@naver.com>
2018-12-18 12:20:12 +0900

    test

---
+++ day02/write_file.py
@@ -0,0 +1,7 @@
+# f = open('ssafy.txt','w') # w: write, r: read, a: append
+# f.write('This is SSAFY!')
+# f.close()
+
+with open('ssafy.txt','w', encoding='utf8') as f:
+    f.write('This is SSAFY!, with 이용했다')
+#close가 필요없음
