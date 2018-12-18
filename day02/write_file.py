0c7dacc96ead4b3984444e1fbed29804de0bc6f0
Young Hoon <kyh2021@naver.com>
2018-12-18 17:50:05 +0900

    day 02

--- day02/write_file.py
+++ day02/write_file.py
@@ -2,6 +2,12 @@
 # f.write('This is SSAFY!')
 # f.close()
 
-with open('ssafy.txt','w', encoding='utf8') as f:
-    f.write('This is SSAFY!, with 이용했다')
+#with open('ssafy.txt','w', encoding='utf8') as f:
+    #f.write('This is SSAFY!, with 이용했다')
 #close가 필요없음
+
+with open('ssafy.txt','w', encoding='utf8') as f:
+    f.writelines(['1\n','2\n','3\n'])
+    #for i in range(10):
+    #    f.write(f'This is \'SSAFY\'! {i}\n')
+    #    #\t : tab, \\ : '\' 문자, \' & \ " : 따옴표, 쌍따옴표 문자
